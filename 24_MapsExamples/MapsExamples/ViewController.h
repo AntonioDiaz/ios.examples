cb07039a9afaecdf77a4904e05ead4d63468bba3
Antonio Díaz Arroyo <antonio.diaz.arroyo@gmal.com>
2017-11-19 12:03:06 +0100

    update mapsExample

--- 24_MapsExamples/MapsExamples/ViewController.h
+++ 24_MapsExamples/MapsExamples/ViewController.h
@@ -1,20 +1,9 @@
-//
-//  ViewController.h
-//  MapsExamples
-//
-//  Created by cice on 16/11/17.
-//  Copyright © 2017 cice. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
 
 @interface ViewController : UIViewController <MKMapViewDelegate, CLLocationManagerDelegate>{
     CLLocationManager *locationManager;
 }
-
-
 @property (weak, nonatomic) IBOutlet MKMapView *mapView;
-
 @end
 
--- 24_MapsExamples/MapsExamples/ViewController.m
+++ 24_MapsExamples/MapsExamples/ViewController.m
@@ -1,16 +1,4 @@
-//
-//  ViewController.m
-//  MapsExamples
-//
-//  Created by cice on 16/11/17.
-//  Copyright © 2017 cice. All rights reserved.
-//
-
 #import "ViewController.h"
-
-@interface ViewController ()
-
-@end
 
 @implementation ViewController
 
@@ -18,63 +6,52 @@
     [super viewDidLoad];
     locationManager = [[CLLocationManager alloc] init];
     [locationManager requestAlwaysAuthorization];
-    
+    //CLLocationManagerDelegate
     locationManager.delegate = self;
     [locationManager startUpdatingLocation];
     //gps precision
     locationManager.desiredAccuracy = kCLLocationAccuracyKilometer;
     //
     locationManager.distanceFilter = 30;
-    
     CLLocation *location = [[CLLocation alloc]
                                    initWithLatitude:locationManager.location.coordinate.latitude
                                    longitude:locationManager.location.coordinate.longitude];
     MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(location.coordinate, 1000, 1000);
     [self.mapView setRegion:region];
+    //MKMapViewDelegate
     self.mapView.delegate = self;
     self.mapView.mapType = MKMapTypeStandard;
     self.mapView.showsScale = true;
     self.mapView.showsCompass = true;
     self.mapView.zoomEnabled = true;
-
     //user location
     self.mapView.showsUserLocation = true;
     self.mapView.userLocation.title = @"Here I am";
     self.mapView.userLocation.subtitle = @"This is Antoine";
-    [self addTumbTack];
+    MKPointAnnotation *pointLope = [self generateAnnotation:40.3204539 withLongitude:-3.7749351 withTitle:@"Lope de Vega" withSubtitle:@"Public School"];
+    MKPointAnnotation *pointSevero = [self generateAnnotation:40.3193087 withLongitude:-3.7725426 withTitle:@"Servero Ochoa" withSubtitle:@"Public Hospital"];
+    MKPointAnnotation *pointUc3m = [self generateAnnotation:40.3310872 withLongitude:-3.7685294 withTitle:@"UC3M" withSubtitle:@"Public University"];
+
+    [self.mapView addAnnotation:pointLope];
+    [self.mapView addAnnotation:pointSevero];
+    [self.mapView addAnnotation:pointUc3m];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
 }
 
+#pragma mark - CLLocationManagerDelegate
 -(void)locationManager:(CLLocationManager *)manager didUpdateLocations:(nonnull NSArray<CLLocation *> *)locations {
     CLLocation *location = [locations objectAtIndex:0];
     [self.mapView setCenterCoordinate:location.coordinate animated:true];
 }
 
-#pragma mark - private methods
--(void) addTumbTack {
-    MKPointAnnotation *pointLopeDeVega = [[MKPointAnnotation alloc] init];
-    pointLopeDeVega.coordinate = CLLocationCoordinate2DMake(40.3204539,-3.7749351);
-    pointLopeDeVega.title = @"Lope de Vega";
-    pointLopeDeVega.subtitle = @"Public School";
-    [self.mapView addAnnotation:pointLopeDeVega];
-    
-    
-    MKPointAnnotation *pointSeveroOchoa = [[MKPointAnnotation alloc] init];
-    pointSeveroOchoa.coordinate = CLLocationCoordinate2DMake(40.3193087,-3.7725426);
-    pointSeveroOchoa.title = @"Severo Ochoa";
-    pointSeveroOchoa.subtitle = @"Public Hosital";
-    [self.mapView addAnnotation:pointSeveroOchoa];
-}
-
 #pragma mark - MKMapViewDelegate
 -(MKAnnotationView*) mapView:(MKMapView *)mapView viewForAnnotation:(id<MKAnnotation>)annotation {
-    //to change the locatio blue point.
+    //to change the location blue point.
     if ([annotation isKindOfClass:[MKUserLocation class]]) {
-        
-    //to change the locatio blue point.
+    //to change the location red point.
     } else if ([annotation isKindOfClass:[MKPointAnnotation class]]) {
         MKAnnotationView *annotationView = [mapView dequeueReusableAnnotationViewWithIdentifier:@"MapPin"];
         if (!annotationView) {
@@ -86,7 +63,6 @@
             
             //boton de la derecha del bocadillo.
             UIButton *buttonRight = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
-            
             [buttonRight addTarget:self action:@selector(clickPin:) forControlEvents:UIControlEventTouchUpInside];
             annotationView.rightCalloutAccessoryView = buttonRight;
             //imagen izq en el bocadillo.
@@ -104,10 +80,23 @@
     return nil;
 }
 
+#pragma mark - private methods
+-(MKPointAnnotation*) generateAnnotation:(CLLocationDegrees) latitude
+                           withLongitude:(CLLocationDegrees) longitude
+                               withTitle:(NSString *) title
+                            withSubtitle:(NSString *) subTitle {
+    MKPointAnnotation *pointAnnotation = [[MKPointAnnotation alloc] init];
+    pointAnnotation.coordinate = CLLocationCoordinate2DMake(latitude, longitude);
+    pointAnnotation.title = title;
+    pointAnnotation.subtitle = subTitle;
+    return pointAnnotation;
+}
+
 -(void)clickPin:(id)sender {
     NSLog(@"click on monument");
 }
 
+#pragma mark - static methods
 + (UIImage *) imageWithImage:(UIImage *) image scatedToSize:(CGSize) newSize {
     UIGraphicsBeginImageContextWithOptions(newSize, false, 0.0);
     [image drawInRect:CGRectMake(0, 0, newSize.width, newSize.height)];
